e540a15b78ae397ba77eb78480af87630d996c97
Sachin Mahajan <sachinm78@gmail.com>
2019-05-20 16:42:13 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
+class ClickbaitValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /(Won't Believe|Secret|Top \d|Guess)/
+      record.errors[attribute] << ("Not a clickbait title")
+    end
+  end
+end
+
 class Post < ActiveRecord::Base
   validates :title, presence: true, clickbait: true
   validates :content, length: { minimum: 250 }
